d3b746462c0ff8caa5e01450b8e42a464475c439
Cormac O'Mahony <cormac@Cormacs-MacBook-Air.local>
2018-10-14 13:19:02 +1100

    Updated snippets.

--- brew-machine-setup.sh
+++ brew-machine-setup.sh
@@ -1,2 +1,18 @@
-brew install bash-completion bash-git-prompt cntlm git maven octave python
-brew cask install intellij-idea-ce macpass macvim slack sourcetree tunnelblick
+# git
+brew install git bash-completion bash-git-prompt
+# Vim
+brew cask install macvim
+# Java/Scala
+brew cask install java
+brew install maven sbt
+
+# IDE's and Devtools
+brew cask install intellij-idea-ce macvim sourcetree slack google-chrome
+# Ack replacement
+brew install the_silver_searcher
+
+# Mosh client for remote SSH
+brew install mosh
+
+# cmake to install youcompletme
+brew install cmake
